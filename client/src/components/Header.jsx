5da01afdfd247676f792e360f40093a7bfb9405a
jaisahdeva <jaisachdeva028@gmail.com>
2024-02-22 14:20:16 -0400

    changed project added functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,14 +6,14 @@ const Header = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        <Link to="/">My University Tasks</Link>
+        <Link to="/">Course Tasks</Link>
       </div>
       <ul className="nav-links">
         <li>
           <Link to="/tasks">Tasks</Link>
         </li>
         <li>
-          <Link to="/add-task">New Task</Link>
+          <Link to="/add-task"> Add A Task</Link>
         </li>
       </ul>
     </nav>
--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "../components/Header";
 import "../styles/Tasks.css";
 import { useTasks } from "../context/TasksContext";
@@ -13,7 +13,10 @@ const Tasks = () => {
       {tasks.length > 0 ? (
         <div className="tasks-grid">
           {tasks.map((task) => (
-            <Task task={task} key={task.id} handleDelete={handleDelete} />
+            <div key={task.id} className="task-item">
+              <Task task={task} handleDelete={() => handleDelete(task.id)} />
+              <div>Date: {task.date}</div> {/* Display the date of the task */}
+            </div>
           ))}
         </div>
       ) : (
